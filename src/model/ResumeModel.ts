8d74d3004d74bb5b7a7694ae4cee953a0237cdd7
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2025-07-05 00:44:48 +0700

    chore: separate schemas

--- src/model/ResumeModel.ts
+++ src/model/ResumeModel.ts
@@ -1,75 +1,9 @@
 import { SupabaseClient } from "@supabase/supabase-js";
-import { z } from "zod";
-
-// resume schema{
-//   owner_id:
-//   template_url?: string
-//   content: {
-//     sections: {
-//       "info":
-//       "education":
-//       "skills":
-//       "experience":
-//     }
-//   }
-//   created_at?: string,
-//   last_updated_at?: string,
-// }
-
-// sample data json for below:
-
-export const ContentSchema = z
-  .object({
-    sections: z
-      .object({
-        info: z
-          .array(
-            z.object({
-              name: z.string(),
-              email: z.string().email().optional(),
-              phone: z.string().optional(),
-              address: z.string().optional(),
-              website: z.string().url().optional(),
-              summary: z.string().optional(),
-            })
-          )
-          .default([]),
-        education: z
-          .array(
-            z.object({
-              institution: z.string().optional(),
-              degree: z.string().optional(),
-              startDate: z.string().optional(),
-              endDate: z.string().optional(),
-            })
-          )
-          .default([]),
-        skills: z.array(z.string()).optional(),
-        experience: z
-          .array(
-            z.object({
-              title: z.string().optional(),
-              companyName: z.string().optional(),
-              startDate: z.string().optional(),
-              endDate: z.string().optional(),
-            })
-          )
-          .default([]),
-      })
-      .default({}),
-  })
-  .default({});
-
-export const ResumeSchema = z.object({
-  owner_id: z.string().uuid(),
-  template_url: z.string().optional(),
-  created_at: z.string(),
-  last_updated_at: z.string(),
-  content: ContentSchema,
-});
-
-export type IResume = z.infer<typeof ResumeSchema>;
-export type IContent = z.infer<typeof ContentSchema>;
+import {
+  IContent,
+  ContentSchema,
+  ResumeSchema,
+} from "@/lib/schemas/resume.schema";
 
 type UpdateableFields = {
   content: IContent;
